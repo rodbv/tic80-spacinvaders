494512b14184b7f98f52b5e9a45e1605f89d8064
Rodrigo Vieira <rodrigo.vieira@gmail.com>
2020-12-19 20:20:18 -0300

    Enemies shooting! 👾

--- spacinvaders.lua
+++ spacinvaders.lua
@@ -10,6 +10,94 @@ function coll(o1, o2)
 		math.abs(o1.y - o2.y) <= game.sprite_w
 end
 
+game = {
+ started = false,
+	res_x = 240,
+	res_y = 136,
+	sprite_w = 8,
+	tics = 0,
+	last_shot = math.huge,
+	press = function(self)
+		if btn(4) then
+		  self.started = true
+		end
+	end,
+	spawn_alien = function(ax, ay, asprite)
+		return {
+			x = ax,
+			y = ay,
+			alive = true,
+			sprite = asprite
+		}
+	end,
+	tic = function(self)
+		self.tics = self.tics + 1		
+	end,
+	shoot = function(self)
+		game.last_shot = game.last_shot + 1
+		if game.last_shot < 20 then 
+			return 
+		end
+		
+		local shooting = 
+					btnp(4,20,20) or btnp(0,20,20)
+		
+		if shooting then
+			game.last_shot = 0
+			sounds:play('shoot')
+			table.insert(
+				shots,
+				{
+					x = ship.x, 
+					y = ship.y-game.sprite_w/2,
+					is_alien = false
+				}
+			)
+		end
+	end,
+	check_hit = function(self)
+		for si,s in ipairs(shots) do
+			if s.is_alien then
+				if coll(s, ship) then
+					sounds:play('hit')
+					sounds:play('hit')
+					sounds:play('hit')
+					print('HIT!!!!')
+				end
+			else
+				for ai,a in ipairs(aliens) do
+					if not s.is_alien and a.alive and coll(a,s) then
+						a.alive = false
+						table.remove(shots,si)
+						sounds:play('hit')
+					end
+				end
+			end
+		end
+	end,
+	init = function(self)
+		for row = 1, 6 do
+			for col=1,6 do
+			 table.insert(
+					aliens,
+					self.spawn_alien(
+						self.res_x // 8 * (col + 1/3),
+						row * 10,
+						sprites.aliens[math.random(1,#sprites.aliens)]
+					)
+				)
+			end
+		end
+	end,
+}
+sprites = {
+	ship = 257,
+	aliens = {256,258,261},
+	black_hole = {259},
+	boss = {260},
+	laser = {263,264},
+}
+
 sounds = {
 	enabled = false,
   toggle = function(self)
@@ -38,88 +126,21 @@ sounds = {
   end
 }
 
-game = {
- started = false,
-	res_x = 240,
-	res_y = 136,
-	sprite_w = 8,
-	tics = 0,
-	last_shot = math.huge,
-	press = function(self)
-		if btn(4) then
-		  self.started = true
-		end
-	end,
-	spawn_alien = function(ax, ay, asprite)
-		return {
-			x = ax,
-			y = ay,
-			sprite = asprite
-		}
-	end,
-	tic = function(self)
-		self.tics = self.tics + 1		
-	end,
-	shoot = function(self)
-		game.last_shot = game.last_shot + 1
-		if game.last_shot < 20 then 
-			return 
-		end
-		
-		local shooting = 
-					btnp(4,20,20) or btnp(0,20,20)
-		
-		if shooting then
-			game.last_shot = 0
-			sounds:play('shoot')
-			table.insert(
-				shots,
-				{ 
-					x = ship.x, 
-					y = ship.y-game.sprite_w/2, 
-				}
-			)
-		end
-	end,
-	check_hit = function(self)
-		for ai,a in ipairs(aliens) do
-			for si,s in ipairs(shots) do
-				if coll(a,s) then
-					table.remove(aliens,ai)
-					table.remove(shots,si)
-					sounds:play('hit')
-				end
-			end
-		end
-	end,
-	init = function(self)
-		for row = 1, 6 do
-			for col=1,6 do
-			 table.insert(
-					aliens,
-					self.spawn_alien(
-						self.res_x // 8 * (col + 1/3),
-						row * 10,
-						sprites.aliens[math.random(1,3)]
-					)
-				)
-			end
-		end
-	end,
-}
-
-sprites = {
-	ship = 257,
-	aliens = {256,258,261},
-	black_hole = {259},
-	boss = {260},
-	laser = {263,264},
-}
 
 shots = {
 	move = function(self)
 		for _,s in ipairs(self) do
-			s.y = s.y-1
+			local offset = s.is_alien and -1 or 1
+			s.y = s.y - offset
+		end
+	end,
+	sprite = function(self, is_alien)
+		if is_alien then return 262 end
+
+		if game.tics%6<3 then 
+			return sprites.laser[1]
+		else
+			return sprites.laser[2]
 		end
 	end,
 	draw = function(self)
@@ -127,9 +148,7 @@ shots = {
 			if s.y < 0 then
 				table.remove(self, i)
 			else
-				local laser = 
-					game.tics%20<10 and 263 or 264 						
-				spr(laser,s.x,s.y,0)
+				spr(s.is_alien and 262 or 263,s.x,s.y,0)
 			end
 		end
 	end,
@@ -140,7 +159,25 @@ aliens = {
 	direction = 1,
 	draw = function(self)
 		for i, a in ipairs(self) do
-			spr(a.sprite,a.x,a.y)
+			if a.alive then
+				spr(a.sprite,a.x,a.y)
+			end
+		end
+	end,
+	fire = function(self)
+		for _, a in ipairs(self) do
+				if 
+					a.alive and 
+					math.random(1,1000) == 666 
+				then
+					table.insert(
+						shots, 
+						{ 
+							x = a.x,
+							y = a.y + game.sprite_w,
+							is_alien = true 
+						})
+				end
 		end
 	end,
 	move = function(self)
@@ -185,18 +222,20 @@ game:init()
 function TIC()
 	cls()
 	if not game.started then
+		spr(sprites.ship, game.res_x/2 - 2* game.sprite_w, 80, -1, 4)
 		print("Press Z to start",80, 50, 3)
 		game:press()
 	 return
 	end
 
-  sounds:toggle()
-  sounds:draw()
+ sounds:toggle()
+ sounds:draw()
 	game:tic()
 	ship:move()
 	ship:draw()
 	aliens:move()
 	aliens:draw()
+	aliens:fire()
 	game:shoot()
 	shots:move()
 	shots:draw()
@@ -210,9 +249,9 @@ end
 -- 003:001ff1000100001010f00f01f00ff00ff00ff00f10f00f0101000010001ff100
 -- 004:00b000b00b08880b0b08b80b0b08880b00b000b0000b0b00000b0b000000b000
 -- 005:0066600006666600060606006677770607777776006666600006600000066600
--- 006:000330000003b000000b30000003b000000b30000003b000000b30000003b000
--- 007:0002200000022000000220000002200000022000000220000002200000022000
--- 008:0004400000044000000440000004400000044000000440000004400000044000
+-- 006:0000000000000000000000000000000000050000000500000005000000050000
+-- 007:0000200000002000000020000000200000002000000020000000200000002000
+-- 008:0000400000004000000040000000400000004000000040000000400000004000
 -- 112:00000000000b0c0000bb00c0bbbbc0c0aabbc0c000ab00c0000a0c0000000000
 -- 113:00000000000d000000dd0000ddddf000eeddf00000ed0000000e000000000000
 -- </SPRITES>
